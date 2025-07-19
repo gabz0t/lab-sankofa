6b9821f313d9e1d401b0616bf4397e358f8a2965
gabz0t <belicogabriel@gmail.com>
2025-07-18 23:21:27 -0300

    ok

--- types.ts
+++ types.ts
@@ -1,22 +1,16 @@
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 37218ee941606064404ce0a43c3ce5b7bf67ba72
 export interface Product {
   id: number;
   name: string;
   description: string;
-<<<<<<< HEAD
   price?: number; // Optional price for digital goods
   imageUrl: string;
   category: 'Limpeza' | 'Ativação' | 'Cura' | 'Proteção' | 'Expansão' | 'Intuição' | 'Verbo' | 'Véu' | 'Presença' | string;
   mediaType?: 'E-book' | 'Áudio Ritual' | 'Ritual Guiado' | 'PDF';
-  
+
   // Optional fields for DigitalProductPage
   pages?: number;
-  content?: string[];
-  ritualSuggestion?: { title: string, steps: string[] };
+  content?: string;
+  ritualSuggestion?: { title: string; steps: string[] };
   excerpt?: string;
   subtitle?: string;
 }
@@ -24,8 +18,4 @@ export interface Product {
 export interface Guardian {
   name: string;
   email: string;
-=======
-  price: number;
-  imageUrl: string;
->>>>>>> 37218ee941606064404ce0a43c3ce5b7bf67ba72
 }
